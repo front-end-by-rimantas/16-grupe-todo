3d70ba539d4db282b9eb7b0fcaf0dc004544ad01
Rimantas <belauzas@yahoo.com>
2020-08-26 19:12:30 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/*
+TODO:
+- LIST
+- ITEM:
+    REQUIRED:
+    - title/description
+    - busena (idea/notes, todo, in progress, done)
+    OPTIONAL:
+    - deadline
+    - labels
+    - assignee
+- FORM
+
+NOTES:
+- inline editing
+*/
